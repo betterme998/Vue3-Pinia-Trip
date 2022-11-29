35c2497b9696e3e4b080022460770c80bc6c7d9d
betterme998 <betterme998@163.com>
2022-11-29 15:56:40 +0800

    .

--- trip/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import "normalize.css";
-import "./assets/css/index.css";
-
-
-createApp(App).use(router).mount('#app')
